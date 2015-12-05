6c9c043f8d10ff6afbbe3ecffc39d09b91a4d325
ndob <dobniki@gmail.com>
2015-12-05 16:03:27 +0200

    Clean up desktop example.

--- example/desktop_webcam/main.cpp
+++ example/desktop_webcam/main.cpp
@@ -1,10 +1,7 @@
 #include "yarrar/Pipeline.h"
 #include "yarrar/dataprovider/WebcamDataProvider.h"
-#include "yarrar/dataprovider/StaticImageDataProvider.h"
 #include "yarrar/detector/MarkerDetector.h"
 #include "yarrar/renderer/opengl/OpenGLRenderer.h"
-#include "yarrar/renderer/dummy/DummyRenderer.h"
-#include "yarrar/renderer/opencv/OpenCVRenderer.h"
 
 int main(int argc, char** argv)
 {
@@ -12,17 +9,13 @@ int main(int argc, char** argv)
 
     Pipeline p;
     p.addDataProvider<WebcamDataProvider>();
-    //p.addDataProvider<StaticImageDataProvider>("/home/niki/programming/yarrar/data/img/marker.jpg");
     p.addDetector<MarkerDetector>();
-    //p.addRenderer<DummyRenderer>();
-    //p.addRenderer<OpenCVRenderer>();
     p.addRenderer<OpenGLRenderer>();
 
     while(true)
     {
         p.run();
         p.runRender();
-        //std::getchar();
     }
 }
 
