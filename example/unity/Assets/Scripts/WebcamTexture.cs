f65bedb41aa574fd9d66993d67cea6e12b1a17e6
ndob <dobniki@gmail.com>
2015-12-14 20:56:19 +0200

    Fix indentation.

--- example/unity/Assets/Scripts/WebcamTexture.cs
+++ example/unity/Assets/Scripts/WebcamTexture.cs
@@ -7,91 +7,91 @@ using SimpleJSON;
 
 public class WebcamTexture : MonoBehaviour 
 {
-	[DllImport ("yarrar")]
-	private static extern IntPtr getPose(IntPtr pixelBuffer, int width, int height);	
+    [DllImport ("yarrar")]
+    private static extern IntPtr getPose(IntPtr pixelBuffer, int width, int height);    
 
-	private WebCamTexture m_webcamTexture;
+    private WebCamTexture m_webcamTexture;
     public RawImage rawimage;
     public GameObject m_camera;
-	public GameObject m_cube;
+    public GameObject m_cube;
 
     void Start()
     {
-		m_webcamTexture = new WebCamTexture();
-		rawimage.texture = m_webcamTexture;
-		rawimage.material.mainTexture = m_webcamTexture;
-		m_webcamTexture.Play();
+        m_webcamTexture = new WebCamTexture();
+        rawimage.texture = m_webcamTexture;
+        rawimage.material.mainTexture = m_webcamTexture;
+        m_webcamTexture.Play();
     }
 
-	void Update() 
-	{
-		Color32[] pixelData = m_webcamTexture.GetPixels32();
-		IntPtr poseDataStringPtr;
-		GCHandle handle = GCHandle.Alloc(pixelData, GCHandleType.Pinned);
-		try
-		{
-			poseDataStringPtr = getPose(handle.AddrOfPinnedObject(), m_webcamTexture.width, m_webcamTexture.height);
-		}
-		finally
-		{
-			handle.Free();
-		}
+    void Update() 
+    {
+        Color32[] pixelData = m_webcamTexture.GetPixels32();
+        IntPtr poseDataStringPtr;
+        GCHandle handle = GCHandle.Alloc(pixelData, GCHandleType.Pinned);
+        try
+        {
+            poseDataStringPtr = getPose(handle.AddrOfPinnedObject(), m_webcamTexture.width, m_webcamTexture.height);
+        }
+        finally
+        {
+            handle.Free();
+        }
 
-		var jsonString = Marshal.PtrToStringAnsi(poseDataStringPtr);
-		var poseData = JSON.Parse(jsonString);
+        var jsonString = Marshal.PtrToStringAnsi(poseDataStringPtr);
+        var poseData = JSON.Parse(jsonString);
 
-		bool valid = poseData["valid"].AsInt != 0 ? true : false;
-		m_cube.SetActive(valid);
+        bool valid = poseData["valid"].AsInt != 0 ? true : false;
+        m_cube.SetActive(valid);
 
-		if(valid)
-		{
-			var camera = m_camera.GetComponent<Camera>();
-			var translation = poseData["translation"].AsArray;
-			var rotationMtx = poseData["rotation"].AsArray;
-			var cameraMatrix = poseData["camera"].AsArray;
+        if(valid)
+        {
+            var camera = m_camera.GetComponent<Camera>();
+            var translation = poseData["translation"].AsArray;
+            var rotationMtx = poseData["rotation"].AsArray;
+            var cameraMatrix = poseData["camera"].AsArray;
 
-			Matrix4x4 viewMatrix = Matrix4x4.identity;
-			// Rotation
-			viewMatrix.m00 = rotationMtx[0].AsFloat;
-			viewMatrix.m01 = rotationMtx[1].AsFloat;
-			viewMatrix.m02 = rotationMtx[2].AsFloat;
-			viewMatrix.m10 = rotationMtx[3].AsFloat;
-			viewMatrix.m11 = rotationMtx[4].AsFloat;
-			viewMatrix.m12 = rotationMtx[5].AsFloat;
-			viewMatrix.m20 = rotationMtx[6].AsFloat;
-			viewMatrix.m21 = rotationMtx[7].AsFloat;
-			viewMatrix.m22 = rotationMtx[8].AsFloat;
+            Matrix4x4 viewMatrix = Matrix4x4.identity;
+            // Rotation
+            viewMatrix.m00 = rotationMtx[0].AsFloat;
+            viewMatrix.m01 = rotationMtx[1].AsFloat;
+            viewMatrix.m02 = rotationMtx[2].AsFloat;
+            viewMatrix.m10 = rotationMtx[3].AsFloat;
+            viewMatrix.m11 = rotationMtx[4].AsFloat;
+            viewMatrix.m12 = rotationMtx[5].AsFloat;
+            viewMatrix.m20 = rotationMtx[6].AsFloat;
+            viewMatrix.m21 = rotationMtx[7].AsFloat;
+            viewMatrix.m22 = rotationMtx[8].AsFloat;
 
-			// Translation
-			viewMatrix.m03 = translation[0].AsFloat;
-			viewMatrix.m13 = translation[1].AsFloat;
-			viewMatrix.m23 = translation[2].AsFloat;
+            // Translation
+            viewMatrix.m03 = translation[0].AsFloat;
+            viewMatrix.m13 = translation[1].AsFloat;
+            viewMatrix.m23 = translation[2].AsFloat;
 
-			// Invert y and z (stored as inverted in OpenCV).
-			Matrix4x4 conversionMatrix = Matrix4x4.identity;
-			conversionMatrix.m00 = 1.0f;
-			conversionMatrix.m11 = -1.0f;
-			conversionMatrix.m22 = -1.0f;
-			conversionMatrix.m33 = 1.0f;
+            // Invert y and z (stored as inverted in OpenCV).
+            Matrix4x4 conversionMatrix = Matrix4x4.identity;
+            conversionMatrix.m00 = 1.0f;
+            conversionMatrix.m11 = -1.0f;
+            conversionMatrix.m22 = -1.0f;
+            conversionMatrix.m33 = 1.0f;
 
-			camera.worldToCameraMatrix = conversionMatrix * viewMatrix;
+            camera.worldToCameraMatrix = conversionMatrix * viewMatrix;
 
-			float fx = cameraMatrix[0].AsFloat;
-			float cx = cameraMatrix[2].AsFloat;
-			float fy = cameraMatrix[4].AsFloat;
-			float cy = cameraMatrix[5].AsFloat;
-	        float far = 10.0f;
-	        float near = 0.1f;
+            float fx = cameraMatrix[0].AsFloat;
+            float cx = cameraMatrix[2].AsFloat;
+            float fy = cameraMatrix[4].AsFloat;
+            float cy = cameraMatrix[5].AsFloat;
+            float far = 10.0f;
+            float near = 0.1f;
 
-			Matrix4x4 projection = Matrix4x4.identity;
-			projection.m00 = fx / cx;
-	        projection.m11 = fy / cy;
-	        projection.m22 = (-1.0f * (far + near) / (far - near));
-	        projection.m23 = (-2.0f * far * near) / (far - near);
-	        projection.m32 = -1;
-	        projection.m33 = 0;
+            Matrix4x4 projection = Matrix4x4.identity;
+            projection.m00 = fx / cx;
+            projection.m11 = fy / cy;
+            projection.m22 = (-1.0f * (far + near) / (far - near));
+            projection.m23 = (-2.0f * far * near) / (far - near);
+            projection.m32 = -1;
+            projection.m33 = 0;
 
-			camera.projectionMatrix = projection;
-		}
+            camera.projectionMatrix = projection;
+        }
     }
 }
