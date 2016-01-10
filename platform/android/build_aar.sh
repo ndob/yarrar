75841cab468ddad44cb62e0a0dc1f41505d1a413
ndob <dobniki@gmail.com>
2016-01-10 14:16:12 +0200

    Early exit on AAR-build if native library build fails.

--- platform/android/build_aar.sh
+++ platform/android/build_aar.sh
@@ -3,6 +3,12 @@ mkdir ../../android_build
 cd ../../android_build
 cmake -DCMAKE_TOOLCHAIN_FILE=../external/android-cmake/android.toolchain.cmake -DANDROID_NDK=$ANDROID_NDK_ROOT -DCMAKE_BUILD_TYPE=Release -DANDROID_ABI="armeabi-v7a with NEON" -DANDROID_NATIVE_API_LEVEL=9 ..
 make
+if [ ! $? -eq 0 ] 
+then
+    echo Error compiling native library. Exiting.
+    exit 1
+fi
+
 mkdir -p ../platform/android/yarrar/src/main/libs/armeabi-v7a/
 cp build/lib/libyarrar.so ../platform/android/yarrar/src/main/libs/armeabi-v7a/
 cd ../platform/android
