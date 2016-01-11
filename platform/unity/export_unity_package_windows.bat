f0320df617d047b36ce81c103e405ed66980d48e
ndob <dobniki@gmail.com>
2016-01-11 22:47:42 +0200

    Improve Unity-package export script.

--- platform/unity/export_unity_package_windows.bat
+++ platform/unity/export_unity_package_windows.bat
@@ -1,4 +1,10 @@
-set UNITY_EXECUTABLE=E:\applications\Unity3d\Editor\Unity.exe
+@echo off
+set UNITY_EXECUTABLE=
 set PROJECT_DIR="%cd%\yarrar"
+
+if "%UNITY_EXECUTABLE%"=="" (
+    echo ERROR: UNITY_EXECUTABLE not set. Fix it by editing this .bat-file and pointing the variable to Unity.exe.
+    exit /b
+)
 
 %UNITY_EXECUTABLE% -batchmode -projectPath %PROJECT_DIR% -exportPackage Assets\yarrar\Plugins Assets\yarrar\Prefabs Assets\yarrar\Scripts yarrar.unitypackage -quit
