9c934d08c458450bb39969ca8088fa6c7f1d254b
ndob <dobniki@gmail.com>
2016-03-15 22:28:16 +0200

    Add kalman filter configuration for VisualWithSensors-fusion.

--- src/yarrar/fusion/vsfusion/VisualWithSensors.cpp
+++ src/yarrar/fusion/vsfusion/VisualWithSensors.cpp
@@ -1,10 +1,44 @@
 #include "VisualWithSensors.h"
 
+namespace
+{
+
+using StateVector = yarrar::VSFusion::StateVector;
+using StateMatrix = yarrar::VSFusion::StateMatrix;
+using MeasurementVector = yarrar::VSFusion::MeasurementVector;
+using MeasurementMatrix = yarrar::VSFusion::MeasurementMatrix;
+using MeasurementUncertaintyMatrix = yarrar::VSFusion::MeasurementUncertaintyMatrix;
+
+const StateVector xInitial = StateVector::Zero();
+const StateVector uInitial = StateVector::Zero();
+const StateMatrix pInitial = StateMatrix::Zero();
+const MeasurementUncertaintyMatrix rInitial = MeasurementUncertaintyMatrix::Zero();
+
+std::tuple<StateVector, StateMatrix> transition(const StateVector& state)
+{
+    StateVector newState = StateVector::Zero();
+    StateMatrix jacobian = StateMatrix::Zero();
+    return std::make_tuple(newState, jacobian);
+}
+
+std::tuple<MeasurementVector, MeasurementMatrix> measurement(const StateVector& state)
+{
+    MeasurementVector newMeasurement = MeasurementVector::Zero();
+    newMeasurement[0] = state[0];
+    newMeasurement[1] = state[1];
+
+    MeasurementMatrix jacobian = MeasurementMatrix::Identity();
+
+    return std::make_tuple(newMeasurement, jacobian);
+}
+}
+
 namespace yarrar
 {
 
 VisualWithSensors::VisualWithSensors(const json11::Json& config)
     : SensorFusion(config)
+    , m_kalman(transition, measurement, xInitial, pInitial, uInitial, rInitial)
 {
 }
 
--- src/yarrar/fusion/vsfusion/VisualWithSensors.h
+++ src/yarrar/fusion/vsfusion/VisualWithSensors.h
@@ -1,9 +1,28 @@
 #pragma once
 
 #include "PipelineStage.h"
+#include "ExtendedKalmanFilter.h"
 
 namespace yarrar
 {
+/*
+ * Kalman filter configuration
+ *
+ * State (13 elements):
+ * omega = angular velocity
+ * q = quaternion orientation
+ * t = position
+ * v = velocity
+ * [omega_x, omega_y, omega_z, q0, q1, q2, q3, t_x, t_y, t_z, v_x, v_y, v_z]
+ *
+ * Measurement (14 elements):
+ * omega = angular velocity from gyroscope
+ * qs = quaternion orientation from sensors
+ * qt = quaternion orientation from visual tracking
+ * t = position from visual tracking
+ * [omega_x, omega_y, omega_z, qs0, qs1, qs2, qs3, qt0, qt1, qt2, qt3, t_x, t_y, t_z]
+ */
+using VSFusion = ExtendedKalmanFilter<13, 14>;
 
 class VisualWithSensors : public SensorFusion
 {
@@ -11,5 +30,8 @@ public:
     VisualWithSensors(const json11::Json& config);
     void getFusedPoses(const std::map<size_t, std::vector<Pose>>& poseLists,
         std::vector<Pose>& output) override;
+
+private:
+    VSFusion m_kalman;
 };
 }
